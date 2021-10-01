3c2e0783e5ed70e518058d6a62d22132c0d23c49
freuziah <90962023+freuziah@users.noreply.github.com>
2021-10-01 18:52:11 +0300

    Add files via upload

---
+++ лабораторка 4 3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a = 0, b = 0;
+    cout << "write a" << "\n";
+    cin >> a;
+    cout << "write b" << "\n";
+    cin >> b;
+    float SA = (a + b) / 2;
+    cout << "The arithmetic mean of a and b is " << SA;
+}
