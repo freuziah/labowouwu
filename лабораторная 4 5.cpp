aca34041ed5f4ecd3d4b8be78cbcfd3ed9f7355e
freuziah <90962023+freuziah@users.noreply.github.com>
2021-10-01 19:18:18 +0300

    Add files via upload

---
+++ лабораторная 4 5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a = 0, b = 0;
+    cout << "write a" << "\n";
+    cin >> a;
+    cout << "write b" << "\n";
+    cin >> b;
+    float a1 = fabs(a);
+    float b1 = fabs(b);
+    if ((a == 0) or (b == 0)) cout << "Check the correctness of the entered data";
+    else cout << "|a| + |b| = " << a1 + b1 << "\n" << "|a| - |b| = " << a1 - b1 << "\n" << "|a| * |b| = " << a1 * b1 << "\n" << "|a| / |b| = " << a1 / b1;
+
+}
