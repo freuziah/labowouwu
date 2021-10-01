5bcfced0ce03b308b0fc3c4678741c3ccbdd0966
freuziah <90962023+freuziah@users.noreply.github.com>
2021-10-01 18:39:47 +0300

    Add files via upload

---
+++ лабораторка 4 2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float d = 0;
+    cout << "Write d" << "\n";
+    cin >> d;
+    float pi = 3.14;
+    float L = d * pi;
+    cout << "The circumference is " << L;
+}
