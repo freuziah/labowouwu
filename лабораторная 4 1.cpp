b1564bc1fcdc11555bf10fd56a6ee49a6ecebf3e
freuziah <90962023+freuziah@users.noreply.github.com>
2021-10-01 17:57:02 +0300

    Add files via upload

---
+++ лабораторная 4 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a = 0, b = 0;
+    cout << "write a" << "\n";
+    cin >> a;
+    cout << "write b" << "\n";
+    cin >> b;
+    float S = a * b;
+    float P = 2 * (a + b);
+    cout << "The area of the rectangle is " << S << "\n";
+    cout << "The perimeter of the rectangle is " << P;
+}
