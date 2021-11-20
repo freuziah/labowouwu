89155476b3078a7076bbceb4e262577d6af9a63f
freuziah <90962023+freuziah@users.noreply.github.com>
2021-11-20 12:33:18 +0300

    Add files via upload

---
+++ лабораторная 8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Task 1" << "\n";
+	int byt;
+	cout << "enter the number of bytes" << "\n";
+	cin >> byt;
+	int kil = byt / 1024;
+	cout << "Answer : " << kil << " kilobyte(s)" << "\n" << "\n";
+
+	cout << "Task 2" << "\n";
+	int A, B;
+	cout << "enter A and B (A > B)" << "\n";
+	cin >> A >> B;
+	int kol = A / B;
+	cout << "Answer : " << kol << " segment(s)" << "\n" << "\n";
+
+	cout << "Task 3" << "\n";
+	cout << "enter A and B (A > B)" << "\n";
+	cin >> A >> B;
+	kol = A / B;
+	int O = A - B * kol;
+	cout << "Answer : " << O << "\n" << "\n";
+
+	cout << "Task 4" << "\n";
+	int C;
+	cout << "enter two-digit number" << "\n";
+	cin >> C;
+	int C1 = C / 10;
+	int C2 = (C - C1 * 10)*10;
+	O = C1 + C2;
+	cout << "Answer : " << O << "\n" << "\n";
+
+
+	cout << "Task 5" << "\n";
+	cout << "enter three-digit number" << "\n";
+	cin >> C;
+	C1 = C / 100;
+	C2 = (C - C1 * 100) * 10;
+	O = C1 + C2;
+	cout << "Answer : " << O << "\n" << "\n";
+}
